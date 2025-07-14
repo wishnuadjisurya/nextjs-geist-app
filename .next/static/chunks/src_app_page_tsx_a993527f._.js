ff620f0cbd76cfb1da96b8c8a2fb3632b14dc5c1
wishnuadjisurya <58412249+wishnuadjisurya@users.noreply.github.com>
2025-07-14 09:43:19 +0800

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1752457398006

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d18ca166._.js",
+  "static/chunks/src_1a44f5b0._.js"
+],
+    source: "dynamic"
+});
